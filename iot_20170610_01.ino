7630b6bb16221803fb85ef0f8cf1cf058ce42820
mashihara <mashiharahiroyuki@gmail.com>
2017-06-10 20:39:51 +0900

    20170610デモ版

--- iot_20170610_01.ino
+++ iot_20170610_01.ino
@@ -7,9 +7,13 @@ const int SERVO_PIN = 10;
 const int BUTTON_PIN = 5;
 const int SENSOR_TRIG_PIN = 6;
 const int SENSOR_ECHO_PIN = 7;
+boolean first = true;
+boolean fuseru = false;
+boolean agaru = false;
 
 //======servo=====
-int pos = 0;    // variable to store the servo position
+int pos = 90;    // variable to store the servo position
+int count = 0;
 
 //===========sensor==========
 double Duration = 0; //受信した間隔
@@ -28,9 +32,9 @@ void setup() {
 
 void loop() {
   //本当はここをfalseではじめたい、けどうまくいかないので後回し
-  boolean button = false;
+  boolean button = true;
   //======button=====
-  if(digitalRead(BUTTON_PIN)==LOW){
+  if(digitalRead(BUTTON_PIN)==HIGH){
     button = true;
   }
   if(button){
@@ -44,16 +48,30 @@ void loop() {
     if (Duration > 0) {
       Duration = Duration/2; //往復距離を半分にする
       Distance = Duration*340*100/1000000; // 音速を340m/sに設定
+      if(Distance < 10){
+        fuseru = true;
+      }else{
+        agaru = true;
+      }
       Serial.print("Distance:");
       Serial.print(Distance);
       Serial.println(" cm");
     }
     //======servo=====
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
+    if(fuseru && agaru){
+      count += 1;
+      pos += count*5;
+      fuseru =false;
+      agaru =false;
     }
+    Serial.println("fuseru:");
+    Serial.println(fuseru);
+    Serial.println("agaru:");
+    Serial.println(agaru);
+    Serial.println("pos:");
+    Serial.println(pos);
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
   }
+  delay(500);
 }
 
